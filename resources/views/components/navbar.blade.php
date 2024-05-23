e255947552e240e848acf4e40583de2e48c50109
Takulian <miftakhulghulam10@gmail.com>
2024-05-23 22:27:39 +0700

    2 page add

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg fixed-top bg-body-tertiary" style="background-color: var(--color-3)">
+  <div class="container-fluid">
+    <a class="navbar-brand mx-3" href="#">
+      <img src="img/Logo.png" alt="" style="width: 9rem">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mx-5" id="navbarNav">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active fw-bold" aria-current="page" href="#" style="color: black; font-size: 25px">Cari Pesanan</a>
+        </li>
+      </ul>
+      <a href="/login">
+        <button class="tombol-login">Masuk</button>
+      </a>
+      
+    </div>
+  </div>
+</nav>
