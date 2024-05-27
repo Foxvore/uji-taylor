2f5e06430bad588983b1200e9e6f08975c1fdba5
“Foxvore” <imjohani2005@gmail.com>
2024-05-27 10:23:04 +0700

    feat: Foreign id configuration

--- database/migrations/2024_05_24_080019_create_pesanans_table.php
+++ database/migrations/2024_05_24_080019_create_pesanans_table.php
@@ -13,22 +13,19 @@ return new class extends Migration
     {
         Schema::create('t_pesanan', function (Blueprint $table) {
             $table->id('id_pesanan');
-            $table->foreignId('kategori_id');
+            $table->foreignId('kategori_id')
+                ->constrained('m_kategori', 'id_kategori')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('kode_pesanan')->unique();
             $table->string('nama_pemesan');
             $table->string('kontak');
             $table->integer('harga');
-            $table->string('referensi')->nullable();
             $table->string('notes')->nullable();
             $table->boolean('status_selesai');
             $table->date('tanggal_pesanan');
             $table->date('estimasi');
             $table->timestamps();
-
-            $table->foreign('kategori_id')
-                ->references('id_kategori')
-                ->on('m_kategori')
-                ->onUpdate('cascade')
-                ->onDelete('cascade');;
         });
     }
 
--- database/migrations/2024_05_24_082214_create_log_selesais_table.php
+++ database/migrations/2024_05_24_082214_create_log_selesais_table.php
@@ -13,15 +13,12 @@ return new class extends Migration
     {
         Schema::create('log_selesai', function (Blueprint $table) {
             $table->id('id_log');
-            $table->foreignId('pesanan_id');
+            $table->foreignId('pesanan_id')
+                ->constrained('t_pesanan', 'id_pesanan')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
             $table->string('keterangan');
             $table->timestamps();
-
-            $table->foreign('pesanan_id')
-                ->references('id_pesanan')
-                ->on('t_pesanan')
-                ->onUpdate('cascade')
-                ->onDelete('cascade');;
         });
     }
 
