9c0d3d778bcb664cfe876396db2e7fb945de1412
“Foxvore” <imjohani2005@gmail.com>
2024-05-28 07:32:13 +0700

    feat: Run admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
+            AdminSeeder::class,
             KategoriSeeder::class,
             PesananSeeder::class
         ]);
