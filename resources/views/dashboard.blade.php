7d7a6fd4d67a4c2c699d399bc01795d809fb465f
“Foxvore” <imjohani2005@gmail.com>
2024-05-28 22:59:48 +0700

    feat: Implement data from database

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,40 +1,40 @@
 @extends('layout.main')
 
 @section('content')
-<x-navbar-admin></x-navbar-admin>
-<div class="bg-polos d-flex">
-    <div class="ringkasan">
-        <div class="fs-2" style="font-weight: 500">Ringkasan</div>
-        <div class="card-ringkasan d-flex flex-column justify-content-center">
-            <div class="fs-2 text-white d-flex" style="font-weight: 500">
-                <div class="me-3">00</div>
-                <div class="me-3 fw-light">|</div>
-                <div>Rp. 300.000</div>
+    <x-navbar-admin></x-navbar-admin>
+    <div class="bg-polos d-flex">
+        <div class="ringkasan">
+            <div class="fs-2" style="font-weight: 500">Ringkasan</div>
+            <div class="card-ringkasan d-flex flex-column justify-content-center">
+                <div class="fs-2 text-white d-flex" style="font-weight: 500">
+                    <div class="me-3">{{ $j_pembuatan }}</div>
+                    <div class="me-3 fw-light">|</div>
+                    <div>Rp. {{ number_format($p_pembuatan, 0, ',', '.') }}</div>
+                </div>
+                <div class="fs-2 text-white fw-light">Total Jahit</div>
             </div>
-            <div class="fs-2 text-white fw-light">Total Jahit</div>
-        </div>
-        <div class="card-ringkasan d-flex flex-column justify-content-center">
-            <div class="fs-2 text-white d-flex" style="font-weight: 500">
-                <div class="me-3">00</div>
-                <div class="me-3 fw-light">|</div>
-                <div>Rp. 300.000</div>
+            <div class="card-ringkasan d-flex flex-column justify-content-center">
+                <div class="fs-2 text-white d-flex" style="font-weight: 500">
+                    <div class="me-3">{{ $j_vermak }}</div>
+                    <div class="me-3 fw-light">|</div>
+                    <div>Rp. {{ number_format($p_vermak, 0, ',', '.') }}</div>
+                </div>
+                <div class="fs-2 text-white fw-light">Total Vermak</div>
             </div>
-            <div class="fs-2 text-white fw-light">Total Vermak</div>
-        </div>
-        <div class="card-ringkasan d-flex flex-column justify-content-center">
-            <div class="fs-2 text-white d-flex" style="font-weight: 500">
-                <div class="me-3">00</div>
-                <div class="me-3 fw-light">|</div>
-                <div>Rp. 300.000</div>
+            <div class="card-ringkasan d-flex flex-column justify-content-center">
+                <div class="fs-2 text-white d-flex" style="font-weight: 500">
+                    <div class="me-3">{{ $j_pembuatan + $j_vermak }}</div>
+                    <div class="me-3 fw-light">|</div>
+                    <div>Rp. {{ number_format($p_pembuatan + $p_vermak, 0, ',', '.') }}</div>
+                </div>
+                <div class="fs-2 text-white fw-light">Total Pendapatan Kotor</div>
             </div>
-            <div class="fs-2 text-white fw-light">Total Pendapatan Kotor</div>
-        </div>
-    
-    </div>
-    <div class="tabel-histori">
-        <div class="fs-2" style="font-weight: 500">Histori Pemesanan</div>
-        <div class="tabel-dumb">Ini tabel nanti gatau kalo gada datanya gimana</div>
-    </div>
 
-</div>
+        </div>
+        <div class="tabel-histori">
+            <div class="fs-2" style="font-weight: 500">Histori Pemesanan</div>
+            <div class="tabel-dumb">Ini tabel nanti gatau kalo gada datanya gimana</div>
+        </div>
+
+    </div>
 @endsection
