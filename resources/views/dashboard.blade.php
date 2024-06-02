ce27a158eb0bd23ed5a8078b4259b288988e262c
“Foxvore” <imjohani2005@gmail.com>
2024-06-02 23:11:49 +0700

    fix: Fix error in passing data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -31,7 +31,7 @@
                     </div>
                     <div class="fs-2 text-white fw-light text-capitalize">Total Pendapatan Kotor</div>
                 </div>
-    
+
             </div>
             <div class="tabel-histori">
                 <div class="fs-2" style="font-weight: 500">Histori Pemesanan</div>
@@ -47,68 +47,63 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($data as $data)
-                            <tr>
-                               <td>{{ $loop->iteration }}</td>
-                               <td>{{ $data->nama_pemesan }}</td>
-                               <td>{{ $data->kontak }}</td>
-                               <td>{{ $data->estimasi }}</td>
-                               <td>
-                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#detailModal{{ $data->id_pesanan }}">
-                                        Detail
-                                    </button>
-                                </td> 
-                            </tr>
-                            {{-- modal --}}
-                            <div class="modal fade" id="detailModal{{ $data->id_pesanan }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog modal-lg">
-                                  <div class="modal-content">
-                                    <div class="modal-header">
-                                      <h1 class="modal-title fs-2" id="exampleModalLabel">Detail Pesanan</h1>
-                                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <div class="d-flex mb-3">
-                                            <div style="width: 100%" class="me-3">
-                                                <label for="inputNama" class="form-label">Nama</label>
-                                                <input type="text" id="inputNama" class="form-control" readonly/>
+                            @foreach ($data_selesai as $ds)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $ds->nama_pemesan }}</td>
+                                    <td>{{ $ds->kontak }}</td>
+                                    <td>{{ $ds->estimasi }}</td>
+                                    <td>
+                                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                            data-bs-target="#detailModal{{ $ds->id_pesanan }}">
+                                            Detail
+                                        </button>
+                                    </td>
+                                </tr>
+                                {{-- modal --}}
+                                <div class="modal fade" id="detailModal{{ $ds->id_pesanan }}" tabindex="-1"
+                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-2" id="exampleModalLabel">Detail Pesanan</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
                                             </div>
-                                            <div style="width: 100%" class="ms-3">
-                                                <label for="inputKategori" class="form-label"
-                                                    >Kategori</label
-                                                >
-                                                <input
-                                                    type="text"
-                                                    id="inputKategori"
-                                                    class="form-control"
-                                                    readonly
-                                                />
+                                            <div class="modal-body">
+                                                <div class="d-flex mb-3">
+                                                    <div style="width: 100%" class="me-3">
+                                                        <label for="inputNama" class="form-label">Nama</label>
+                                                        <input type="text" id="inputNama" class="form-control"
+                                                            readonly />
+                                                    </div>
+                                                    <div style="width: 100%" class="ms-3">
+                                                        <label for="inputKategori" class="form-label">Kategori</label>
+                                                        <input type="text" id="inputKategori" class="form-control"
+                                                            readonly />
+                                                    </div>
+                                                </div>
+                                                <div class="d-flex mb-3">
+                                                    <div style="width: 100%" class="me-3">
+                                                        <label for="inputPesanan" class="form-label">Kode Pesanan</label>
+                                                        <input type="text" id="inputPesanan" class="form-control"
+                                                            readonly />
+                                                    </div>
+                                                    <div style="width: 100%" class="ms-3">
+                                                        <label for="inputHarga" class="form-label">Harga</label>
+                                                        <input type="text" id="inputHarga" class="form-control"
+                                                            readonly />
+                                                    </div>
+                                                </div>
+                                                <div class="mb-4">
+                                                    <label for="inputCatatan" class="form-label">Catatan</label>
+                                                    <textarea name="" id="inputCatatan" class="form-control" readonly></textarea>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="d-flex mb-3">
-                                            <div style="width: 100%" class="me-3">
-                                                <label for="inputPesanan" class="form-label">Kode Pesanan</label>
-                                                <input type="text" id="inputPesanan" class="form-control" readonly/>
-                                            </div>
-                                            <div style="width: 100%" class="ms-3">
-                                                <label for="inputHarga" class="form-label">Harga</label>
-                                                <input type="text" id="inputHarga" class="form-control" readonly/>
-                                            </div>
-                                        </div>
-                                        <div class="mb-4">
-                                            <label for="inputCatatan" class="form-label">Catatan</label>
-                                            <textarea
-                                                name=""
-                                                id="inputCatatan"
-                                                class="form-control"
-                                                readonly
-                                            ></textarea>
                                         </div>
                                     </div>
-                                  </div>
                                 </div>
-                              </div>
-                              {{-- modal --}}
+                                {{-- modal --}}
                             @endforeach
                         </tbody>
                     </table>
